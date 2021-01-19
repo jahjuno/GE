b374fe478e3d645970f83f329d01310587592660
jahjuno <aina.juno.rafidison@esti.mg>
2021-01-19 18:10:44 +0300

    modification acceuil_admin

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import eel
+from os import environ
+from psutil import net_connections
 
 eel.init('src')
 
+#def main():
+	#eel.start('index.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron.exe', '.'])
+
+def voirPort(port):
+	#vérification port
+	for proc in net_connections():
+		if proc.laddr.port == port : return True
+
 def main():
-	eel.start('index.html', mode='custom', cmdline_args=['node_modules/electron/dist/electron.exe', '.'])
+	PORT = 1333
+	while voirPort(PORT):
+		PORT += 1
+	environ['gePORT'] = str(PORT)
+	eel.start(mode='custom', cmdline_args=['node_modules/electron/dist/electron.exe', '.'], port=PORT)
 
 
 if __name__ == '__main__':
